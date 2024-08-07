ca55389dde37498f1ed7e24b49533d5a39abac31
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-08-07 17:50:57 +0330

    colum left

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
-	"image/color"
 	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+
+	/*"image/color"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/dialog"*/
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+func openNewWindow(a fyne.App, title string) {
+	// ایجاد پنجره جدید
+	newWindow := a.NewWindow(title)
+
+	// اضافه کردن محتوا به پنجره جدید
+	label := widget.NewLabel("You selected: " + title)
+	newWindow.SetContent(container.NewVBox(label))
+
+	// تنظیم اندازه و نمایش پنجره جدید
+	newWindow.Resize(fyne.NewSize(1000, 600))
+	newWindow.CenterOnScreen()
+	newWindow.Show()
+}
 
 func main() {
 	myApp := app.New()
@@ -25,110 +40,137 @@ func main() {
 
 	myWindow.SetIcon(iconResource)
 
-	createSeparator := func() *canvas.Line {
-		line := canvas.NewLine(color.Black)
-		line.StrokeWidth = 1
-		return line
-	}
+	//---------------------------------------------- right -----------------------------------------------
+	/*
+		createSeparator := func() *canvas.Line {
+			line := canvas.NewLine(color.Black)
+			line.StrokeWidth = 1
+			return line
+		}
 
-	// --- Items -----
-	item1 := widget.NewLabel("Item 1")
-	separator1 := createSeparator()
+		// --- Items -----
+		item1 := widget.NewLabel("Item 1")
+		separator1 := createSeparator()
 
-	item2 := widget.NewLabel("Item 2")
+		item2 := widget.NewLabel("Item 2")
 
-	// --- Select -----
-	combo := widget.NewSelect([]string{"Option 1", "Option 2"}, func(value string) {
+		// --- Select -----
+		combo := widget.NewSelect([]string{"Option 1", "Option 2"}, func(value string) {
+			log.Println("Select set to", value)
+		})
+
+		// --- Select And Type -----
+		options := []string{"Option 1", "Option 2", "Option 3"}
+		selectEntry := widget.NewSelectEntry(options)
+
+		selectEntry.SetText("Option 2")
+
+		selectEntry.OnChanged = func(s string) {
+			fmt.Println("Changed to:", s)
+		}
+
+		// --- Check Box Normal -----
+		check := widget.NewCheck("Check", func(value bool) {
+			fmt.Println("Check : ", value)
+		})
+
+		// --- Check Box Disable -----
+		disableCheck := widget.NewCheck("disableCheck", func(value bool) {
+			fmt.Println("disableCheckprint :", value)
+		})
+		disableCheck.Disable()
+
+		// --- Check Box Group -----
+		option := []string{"Mashhad", "Tehran", "Esfahan"}
+		checkBoxes := []*widget.Check{}
+		for _, m := range option {
+
+			groupCheck := widget.NewCheck(m, func(value bool) {
+				fmt.Println("Group Check Box : ", value)
+			})
+			checkBoxes = append(checkBoxes, groupCheck)
+		}
+
+		horizontalContainer := container.NewHBox()
+		for _, checkBox := range checkBoxes {
+			horizontalContainer.Add(checkBox)
+		}
+
+		// --- Redio Group -----
+		RadionOption := []string{"Football", "Basketball", "Baceball"}
+		var radioButtons []fyne.CanvasObject
+		for _, option := range RadionOption {
+			radio := widget.NewRadioGroup([]string{option}, func(value string) {
+				fmt.Println("Radio : ", value)
+			})
+			radioButtons = append(radioButtons, radio)
+		}
+		radioContainer := container.NewGridWithColumns(len(radioButtons), radioButtons...)
+
+		// --- Radio Disable -----
+		disableRadioItem := []string{"wrestling", "swimming"}
+		var radioDisableButtons []fyne.CanvasObject
+
+		for _, h := range disableRadioItem {
+
+			disableRadio := widget.NewRadioGroup([]string{h}, func(sport string) {
+				fmt.Println("disable : ", sport)
+			})
+
+			disableRadio.Disable()
+			radioDisableButtons = append(radioDisableButtons, disableRadio)
+
+		}
+		radioContainerDisable := container.NewGridWithColumns(len(radioDisableButtons)*2, radioDisableButtons...)
+
+		// --- Navbar Slider -----
+
+		Slider := widget.NewSlider(0, 100)
+		Slider.SetValue(20)
+
+		// --- Select File -----
+
+		pathEntry := widget.NewEntry()
+		pathEntry.SetPlaceHolder("No folder selected")
+
+		openButton := widget.NewButton("Open Folder", func() {
+			folderDialog := dialog.NewFolderOpen(func(dir fyne.ListableURI, err error) {
+				if err != nil {
+					fmt.Println("Error opening folder:", err)
+					return
+				}
+				if dir == nil {
+					fmt.Println("No folder selected")
+					return
+				}
+				pathEntry.SetText(dir.Path())
+			}, myWindow)
+			folderDialog.Show()
+		})
+
+
+	*/
+	//---------------------------------------------- left -----------------------------------------------
+	// --- buttonPlus -----
+
+	plus := widget.NewSelect([]string{"levelDB", "DynamoDB", "SQL", "DucomentDB"}, func(value string) {
 		log.Println("Select set to", value)
+		openNewWindow(myApp, value)
 	})
+	plus.PlaceHolder = "Data Source"      // تغییر متن Placeholder
+	plus.Alignment = fyne.TextAlignCenter // مرکز کردن متن
 
-	// --- Select And Type -----
-	options := []string{"Option 1", "Option 2", "Option 3"}
-	selectEntry := widget.NewSelectEntry(options)
-
-	selectEntry.SetText("Option 2")
-
-	selectEntry.OnChanged = func(s string) {
-		fmt.Println("Changed to:", s)
-	}
-
-	// --- Check Box Normal -----
-	check := widget.NewCheck("Check", func(value bool) {
-		fmt.Println("Check : ", value)
+	buttonOne := widget.NewButton("DDL data source", func() {
+		fmt.Println("buttonOne")
 	})
-
-	// --- Check Box Disable -----
-	disableCheck := widget.NewCheck("disableCheck", func(value bool) {
-		fmt.Println("disableCheckprint :", value)
+	buttonTwo := widget.NewButton("Data source from URL", func() {
+		fmt.Println("buttonTwo")
 	})
-	disableCheck.Disable()
-
-	// --- Check Box Group -----
-	option := []string{"Mashhad", "Tehran", "Esfahan"}
-	checkBoxes := []*widget.Check{}
-	for _, m := range option {
-
-		groupCheck := widget.NewCheck(m, func(value bool) {
-			fmt.Println("Group Check Box : ", value)
-		})
-		checkBoxes = append(checkBoxes, groupCheck)
-	}
-
-	horizontalContainer := container.NewHBox()
-	for _, checkBox := range checkBoxes {
-		horizontalContainer.Add(checkBox)
-	}
-
-	// --- Redio Group -----
-	RadionOption := []string{"Football", "Basketball", "Baceball"}
-	var radioButtons []fyne.CanvasObject
-	for _, option := range RadionOption {
-		radio := widget.NewRadioGroup([]string{option}, func(value string) {
-			fmt.Println("Radio : ", value)
-		})
-		radioButtons = append(radioButtons, radio)
-	}
-	radioContainer := container.NewGridWithColumns(len(radioButtons), radioButtons...)
-
-	// --- Radio Disable -----
-	disableRadioItem := []string{"wrestling", "swimming"}
-	var radioDisableButtons []fyne.CanvasObject
-
-	for _, h := range disableRadioItem {
-
-		disableRadio := widget.NewRadioGroup([]string{h}, func(sport string) {
-			fmt.Println("disable : ", sport)
-		})
-
-		disableRadio.Disable()
-		radioDisableButtons = append(radioDisableButtons, disableRadio)
-
-	}
-	radioContainerDisable := container.NewGridWithColumns(len(radioDisableButtons)*2, radioDisableButtons...)
-
-	// --- Navbar Slider -----
-
-	Slider := widget.NewSlider(0, 100)
-	Slider.SetValue(20)
-
-	// --- Select File -----
-
-	pathEntry := widget.NewEntry()
-	pathEntry.SetPlaceHolder("No folder selected")
-
-	openButton := widget.NewButton("Open Folder", func() {
-		folderDialog := dialog.NewFolderOpen(func(dir fyne.ListableURI, err error) {
-			if err != nil {
-				fmt.Println("Error opening folder:", err)
-				return
-			}
-			if dir == nil {
-				fmt.Println("No folder selected")
-				return
-			}
-			pathEntry.SetText(dir.Path())
-		}, myWindow)
-		folderDialog.Show()
+	buttonThree := widget.NewButton("Data source from path", func() {
+		fmt.Println("buttonThree")
+	})
+	buttonFor := widget.NewButton("Import from Clipboard", func() {
+		fmt.Println("buttonFor")
 	})
 
 	// --- Bottuns Light And Dark -----
@@ -141,87 +183,45 @@ func main() {
 		myApp.Settings().SetTheme(theme.LightTheme())
 	})
 
-	darkLight := container.NewGridWithColumns(2, lightBottum, darkBottum)
-
-	// --- buttonPlus -----
-
-	plusButton := widget.NewButton("+", func() {
-		// وقتی دکمه کلیک شد، منو باز شود
-		item1 := fyne.NewMenuItem("Data Source", nil)
-		item2 := fyne.NewMenuItem("Details", nil)
-		item3 := fyne.NewMenuItem("Home", nil)
-		item4 := fyne.NewMenuItem("Run", nil)
-
-		// منوی فرزند برای آیتم اول
-		item1.ChildMenu = fyne.NewMenu(
-			"", // برچسب خالی
-			fyne.NewMenuItem("LevelBD", func() {
-				filter := storage.NewExtensionFileFilter([]string{".ldb"})
-
-				fileDialog := dialog.NewFileOpen(func(uri fyne.URIReadCloser, err error) {
-					if err != nil || uri == nil {
-						return
-					}
-					defer uri.Close()
-					// انجام عملیات با فایل انتخاب شده
-					filePath := uri.URI().Path()
-					dialog.ShowInformation("Selected File", filePath, myWindow)
-				}, myWindow)
-
-				fileDialog.SetFilter(filter)
-				fileDialog.Show()
-			}), // آیتم‌های منوی فرزند
-			fyne.NewMenuItem("MongoDB", nil),
-			fyne.NewMenuItem("MYSQL", nil),
-		)
-
-		// منوی اصلی
-		mainMenu := fyne.NewMainMenu(
-			fyne.NewMenu("File", item1, item2, item3, item4),
-			fyne.NewMenu("Help",
-				fyne.NewMenuItem("About", func() {
-					fyne.CurrentApp().SendNotification(&fyne.Notification{
-						Title:   "About",
-						Content: "This is a simple Fyne application",
-					})
-				}),
-			),
-		)
-
-		// تنظیم منوی اصلی برای پنجره
-		myWindow.SetMainMenu(mainMenu)
-	})
+	darkLight := container.NewVBox(
+		layout.NewSpacer(),
+		container.NewGridWithColumns(2, lightBottum, darkBottum),
+	)
 
 	// --- Colunm Right -----
 	rightColumnContent := container.NewVBox(
-		item1,
-		separator1,
-		item2,
-		combo,
-		selectEntry,
-		check,
-		disableCheck,
-		horizontalContainer,
-		radioContainer,
-		radioContainerDisable,
-		Slider,
-		pathEntry,
-		openButton,
-		darkLight,
+	/*item1,
+	separator1,
+	item2,
+	combo,
+	selectEntry,
+	check,
+	disableCheck,
+	horizontalContainer,
+	radioContainer,
+	radioContainerDisable,
+	Slider,
+	pathEntry,
+	openButton,*/
 	)
 
 	// --- Colunm Left -----
 
 	lastColumnContent := container.NewVBox(
-		plusButton,
+		plus,
+		buttonOne,
+		buttonTwo,
+		buttonThree,
+		buttonFor,
+		darkLight,
 	)
 
 	columns := container.NewHSplit(lastColumnContent, rightColumnContent)
 	columns.SetOffset(0.25)
 
 	scrol := container.NewScroll(columns)
-
+	myWindow.CenterOnScreen()
 	myWindow.SetContent(scrol)
-	myWindow.Resize(fyne.NewSize(900, 600))
+	myWindow.Resize(fyne.NewSize(1500, 1000))
 	myWindow.ShowAndRun()
 }
