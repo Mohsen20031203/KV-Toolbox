4bbc61e9bb0f9e61128ac05172fbd6afe957dab4
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-10-17 06:07:55 +0330

    edit struct and func New redis
    edit struct RedisDatabase and func NewDataBaseRedis

--- internal/Databaces/Redis/Redis.go
+++ internal/Databaces/Redis/Redis.go
@@ -1,50 +1,74 @@
 package redisdb
 
 import (
+	"context"
+	"fmt"
 	dbpak "testgui/internal/Databaces"
 	"testgui/internal/Databaces/itertor"
 	iterRedis "testgui/internal/Databaces/itertor/Redis"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type RedisDatabase struct {
-	Host     float32
-	Port     int16
-	Username string
-	Password string
-	Timeout  uint8
+	client    *redis.Client
+	ctx       context.Context
+	Host      string
+	Port      int
+	DataAlias int
+	Username  string
+	Password  string
+	Timeout   uint8
 }
 
-func NewDataBaseRedis(host float32, port int16, user string, password string, timeot uint8) dbpak.DBClient {
+func NewDataBaseRedis(host string, port int, dataAlias int, username string, password string, timeout uint8) dbpak.DBClient {
 	return &RedisDatabase{
-		Host:     host,
-		Port:     port,
-		Username: user,
-		Password: password,
-		Timeout:  timeot,
+		Host:      host,
+		Port:      port,
+		DataAlias: dataAlias,
+		Username:  username,
+		Password:  password,
+		Timeout:   timeout,
 	}
 }
 
 func (r *RedisDatabase) Open() error {
-	return nil
+	addr := fmt.Sprintf("%s:%d", r.Host, r.Port)
+
+	r.client = redis.NewClient(&redis.Options{
+		Addr:        addr,
+		Username:    r.Username,
+		Password:    r.Password,
+		DB:          r.DataAlias,
+		DialTimeout: time.Duration(r.Timeout),
+	})
+	r.ctx = context.Background()
+
+	_, err := r.client.Ping(r.ctx).Result()
+	return err
 }
 
 func (r *RedisDatabase) Add(key string, value string) error {
-	return nil
+	return r.client.Set(r.ctx, key, value, 0).Err()
 }
 
 func (r *RedisDatabase) Close() {
-
+	r.Close()
 }
 
 func (r *RedisDatabase) Delete(key string) error {
-	return nil
+	return r.client.Del(r.ctx, key).Err()
 }
 
 func (r *RedisDatabase) Get(key string) (string, error) {
-	return "", nil
+	return r.client.Get(r.ctx, key).Result()
 }
 
 func (r *RedisDatabase) Iterator(start, end *string) itertor.IterDB {
+
+	r.client.Scan(r.ctx, 0, "*", 10)
+
 	return &iterRedis.RedisIter{}
 }
 
