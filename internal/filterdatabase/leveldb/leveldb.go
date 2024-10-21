a29657acebdca1338fa6574cd485864f7641ce66
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-10-21 10:15:35 +0330

    add func FormatFilesDatabase in package sharedfunc

--- internal/filterdatabase/leveldb/leveldb.go
+++ internal/filterdatabase/leveldb/leveldb.go
@@ -1,10 +1,6 @@
 package FilterLeveldb
 
 import (
-	"io/ioutil"
-	"log"
-	"path/filepath"
-	"strings"
 	"testgui/internal/filterdatabase"
 	sharedfunc "testgui/internal/filterdatabase/SharedFunc"
 
@@ -21,22 +17,7 @@ func NewFileterLeveldb() filterdatabase.FilterData {
 }
 
 func (l *NameDatabaseLeveldb) FilterFile(path string) bool {
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		log.Fatal("Error reading folder:", err)
-		return false
-	}
-	var count uint8
-	for _, file := range files {
-		if strings.HasPrefix(file.Name(), "MANIFEST-") || filepath.Ext(file.Name()) == ".log" {
-			count++
-		}
-
-		if count == 2 {
-			return true
-		}
-	}
-	return false
+	return sharedfunc.FormatFilesDatabase(path)
 }
 
 func (l *NameDatabaseLeveldb) FilterFormat(folderDialog *dialog.FileDialog) {
